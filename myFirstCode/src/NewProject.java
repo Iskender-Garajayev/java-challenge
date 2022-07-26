779e9c19328f2caa77ea00be496baff0bd2abe25
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-26 07:07:39 +0300

    update

--- myFirstCode/src/NewProject.java
+++ myFirstCode/src/NewProject.java
@@ -1,7 +1,8 @@
 public class NewProject {
     public static void main(String[] args) {
     //    let a = 35;
-        System.out.println("Hello World");
+//        let a = 12+12;
+        System.out.println("a");
 
     }
 
